f43a6871494095147191a2ab982f9dc491a8f6f3
HarishK-CS <harishk.cs1@gmail.com>
2023-04-25 18:31:16 +0530

    Banner Ad Added.

--- app/src/main/java/com/techcos/only4g5g/MainActivity.java
+++ app/src/main/java/com/techcos/only4g5g/MainActivity.java
@@ -1,5 +1,6 @@
 package com.techcos.only4g5g;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.Toolbar;
@@ -10,16 +11,37 @@ import android.text.Html;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.LoadAdError;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.rewarded.RewardedAd;
+import com.google.android.gms.ads.rewarded.RewardedAdLoadCallback;
+
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     AppCompatButton btn1,btn2;
+    private AdView mAdView;
+    private RewardedAd rewardedAd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        mAdView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         btn1=findViewById(R.id.Btn1);
         btn2=findViewById(R.id.Btn2);
@@ -52,4 +74,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+
 }
